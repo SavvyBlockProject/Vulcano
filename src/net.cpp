95dc9704d928f5be2ad5e94a725e418863b360e5
zero24x <zero@you-spam.com>
2018-05-30 02:27:04 +0200

    hardcode latest seed nodes

--- src/net.cpp
+++ src/net.cpp
@@ -1229,7 +1229,8 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"198.136.28.100", "198.136.28.100"},
+    {"vulcano-seednode-1.dynu.net", "vulcano-seednode-1.dynu.net"},
+	{"vulcano-seednode-2.dynu.net", "vulcano-seednode-2.dynu.net"},
 };
 
 void ThreadDNSAddressSeed(void* parg)

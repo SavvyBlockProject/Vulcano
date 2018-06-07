ff9a5c600110122efc51f4a52294dd7b8755afba
zero24x <zero@you-spam.com>
2018-06-07 22:42:44 +0200

    max 10.6 compat fixes

---
+++ build_osx.sh
@@ -0,0 +1,5 @@
+qmake RELEASE=1 USE_UPNP=1 USE_QRCODE=1 USE_IPV6=1 VULCANO-qt.pro
+make
+export QTDIR=/opt/local/libexec/qt4/share
+T=$(contrib/qt_translations.py $QTDIR/translations src/qt/locale)
+python2.7 contrib/macdeploy/macdeployqtplus -add-qt-tr $T -dmg -fancy contrib/macdeploy/fancy.plist Vulcano.app
